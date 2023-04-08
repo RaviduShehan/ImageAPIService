2d9712a4207a3696918de5daa6b90274f53cf829
Ravidu Perera <sheahn20133@gmail.com>
2023-04-08 22:55:37 +0530

    Add files

---
+++ ImageService.py
@@ -0,0 +1,26 @@
+import openai
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route('/image')
+def image():
+    prompt = request.args.get('prompt')
+    if not prompt:
+        return jsonify(error="Prompt parameter is missing"), 400
+
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="512x512",
+            response_format="url"
+        )
+        return jsonify(response=response['data'][0]['url'])
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5004)
