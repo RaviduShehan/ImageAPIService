565c84502f302dd2a4e79b1017abcc74ba0ff3e0
Ravidu Perera <sheahn20133@gmail.com>
2023-04-12 03:08:05 +0530

    Add API key as env variable

--- src/ImageService.py
+++ src/ImageService.py
@@ -2,7 +2,7 @@ import openai
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 @app.route('/image')
 def image():
