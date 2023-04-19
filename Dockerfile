a37adbd9764625f5244177a55fc50f438e3fd369
Ravidu Perera <sheahn20133@gmail.com>
2023-04-19 22:49:19 +0530

    Add Files for CI CD

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
 FROM python:buster
 WORKDIR /ImageApp
 COPY requirements.txt .
+COPY src/serviceAccountKey.json .
 RUN pip install -r requirements.txt
+
 COPY src src
+# Set environment variables for Firestore connection
+ENV GOOGLE_APPLICATION_CREDENTIALS="./serviceAccountKey.json"
+RUN pip install google-cloud-firestore
+RUN pip install firebase-admin
+
 EXPOSE 5004
 ENTRYPOINT ["python", "./src/ImageService.py"]
--- src/ImageService.py
+++ src/ImageService.py
@@ -1,16 +1,38 @@
-import openai
-from flask import Flask, jsonify, request
+from datetime import datetime
 import os
+from flask import Flask, request, jsonify
+import openai
+from google.cloud import firestore
+import firebase_admin
+from firebase_admin import credentials
+
+#setup firebase credentials
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+# Initialize Firestore client with explicit project ID
+project_id = "apiservices-384019"
+db = firestore.Client(project=project_id)
 
-@app.route('/image')
+
+@app.route('/')
 def image():
+    # Save service name, status, and timestamp to Firestore
+    service_ref = db.collection('Services').document('ImageService_Status')
+    print("ImageAPI Service Started.....")
+    service_data = {
+        'service_name': 'Image',
+        'status': 'Starting',
+        'timestamp': datetime.now()
+    }
+    service_ref.set(service_data)
     prompt = request.args.get('prompt')
     if not prompt:
+        service_ref.update({'status': 'Empty Prompt'})
         return jsonify(error="Prompt parameter is missing"), 400
-
+    service_ref.update({'status': 'Running'})
     try:
         response = openai.Image.create(
             prompt=prompt,
@@ -20,6 +42,7 @@ def image():
         )
         return jsonify(response=response['data'][0]['url'])
     except Exception as e:
+        service_ref.update({'status': 'Error'})
         return jsonify(error=str(e)), 500
 
 
